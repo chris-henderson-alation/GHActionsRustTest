c1be82fcda6a2599dc292889882abe75d341fd17
christopher-henderson <chris@chenderson.org>
2021-09-28 14:59:17 -0700

    test the push

--- sweep_the_leg.sh
+++ sweep_the_leg.sh
@@ -8,7 +8,7 @@ FROM alpine:latest
 ENTRYPOINT ["/bin/echo", "Hello world"]
 EOF
 
-docker login --username AWS --password-stdin "$(aws ecr get-login-password --region us-east-2)" 248135293344.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 248135293344.dkr.ecr.us-east-2.amazonaws.com
 
 docker build -t "${REFERENCE}" .
 docker push "${REFERENCE}"
