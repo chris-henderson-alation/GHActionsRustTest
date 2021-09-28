bf90f9056cd419d35a2d6ae962894871eca00b94
christopher-henderson <chris@chenderson.org>
2021-09-28 14:50:01 -0700

    test the push

--- sweep_the_leg.sh
+++ sweep_the_leg.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.2.41.zip" -o "awscliv2.zip"
-unzip -q awscliv2.zip
-sudo ./aws/install --update
-#alias aws=/usr/local/bin/aws
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.2.41.zip" -o "awscliv2.zip"
+#unzip -q awscliv2.zip
+#sudo ./aws/install --update
+##alias aws=/usr/local/bin/aws
+#
+#mkdir -p ~/.aws/
+#
+#cat > ~/.aws/credentials <<EOF
+#[ocfbuild]
+#AWS_ACCOUNT="${AWS_ACCOUNT}"
+#AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+#AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+#AWS_REGION="${AWS_REGION}"
+#EOF
+#
+#cat ~/.aws/credentials
 
-mkdir -p ~/.aws/
-
-cat > ~/.aws/credentials <<EOF
-[ocfbuild]
-AWS_ACCOUNT="${AWS_ACCOUNT}"
-AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
-AWS_REGION="${AWS_REGION}"
-EOF
-
-cat ~/.aws/credentials
-
-PASSWORD=$(aws ecr get-login-password --region "${AWS_REGION}" --profile ocfbuild)
+PASSWORD=$(aws ecr get-login-password --region us-east-2 --profile ocfbuild)
 
 #aws set AWS_ACCOUNT "${AWS_ACCOUNT}"
 #aws set AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
