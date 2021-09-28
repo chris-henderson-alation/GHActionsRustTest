70ba81c37e8ca45ab4bb7ad30a19becfc6a8e0a4
christopher-henderson <chris@chenderson.org>
2021-09-28 14:14:27 -0700

    test the push

--- sweep_the_leg.sh
+++ sweep_the_leg.sh
@@ -10,7 +10,7 @@ aws configure set AWS_ACCOUNT "${AWS_ACCOUNT}"
 aws configure set AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
 aws configure set AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
 aws configure set AWS_REGION "${AWS_REGION}"
-PASSWORD=$(aws ecr get-login-password)
+PASSWORD=$(aws ecr get-login-password --region east-us-2)
 
 TAG=just-a-test
 REFERENCE=248135293344.dkr.ecr.us-east-2.amazonaws.com/ocf-system/acm:"${TAG}"
