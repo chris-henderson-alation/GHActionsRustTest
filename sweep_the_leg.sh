f00099a624ae1870811e848b45dd5ae935223b58
christopher-henderson <chris@chenderson.org>
2021-09-29 14:08:42 -0700

    asfasda

--- sweep_the_leg.sh
+++ sweep_the_leg.sh
@@ -8,7 +8,7 @@ FROM alpine:latest
 ENTRYPOINT ["/bin/echo", "Hello world"]
 EOF
 
-echo "some other changeasd"
+echo "some other changead"
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 248135293344.dkr.ecr.us-east-2.amazonaws.com
 
