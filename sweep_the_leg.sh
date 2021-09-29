adbf23458abb33f9487b9017fa773ce785919e18
christopher-henderson <chris@chenderson.org>
2021-09-29 06:50:04 -0700

    please load the cache

--- sweep_the_leg.sh
+++ sweep_the_leg.sh
@@ -8,6 +8,8 @@ FROM alpine:latest
 ENTRYPOINT ["/bin/echo", "Hello world"]
 EOF
 
+echo "some change"
+
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 248135293344.dkr.ecr.us-east-2.amazonaws.com
 
 docker build -t "${REFERENCE}" .
